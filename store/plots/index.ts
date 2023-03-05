c8113daa143aaf061926ea8e4492c734c17fc800
v3nt <dan@jynk.net>
2023-03-05 13:56:37 +0000

    init setup and installation

---
+++ store/plots/index.ts
@@ -0,0 +1,14 @@
+
+export const usePostsStore = defineStore("plots", {
+    state: () => {
+        return {
+            plotsList: [],
+            plot: null,
+        };
+    },
+    actions: {
+    },
+    getters: {
+
+    },
+});
